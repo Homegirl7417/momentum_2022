b25d0c882157b95f62a40417a8add4b743ebab2c
ParkHyunKyeong <kj3477417@gmail.com>
2022-08-18 22:56:52 +0900

    feature: add Clock

--- js/greetings.js
+++ js/greetings.js
@@ -5,7 +5,7 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function onLoginSubmit (event) {
+function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
@@ -13,7 +13,7 @@ function onLoginSubmit (event) {
     paintGreetings(username);
 }
 
-function paintGreetings (username) {
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
