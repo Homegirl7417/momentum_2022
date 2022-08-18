53217e0275dd7970fe57ba72129aa38f0d4aeedf
ParkHyunKyeong <kj3477417@gmail.com>
2022-08-19 00:08:13 +0900

    feature: add background js - add background images

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const images = ["1.jpeg", "2.jpeg", "3.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
