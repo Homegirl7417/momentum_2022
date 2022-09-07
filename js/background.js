c63bfe76eeee88959005d332417a1e284b50a3df
ParkHyunKyeong <kj3477417@gmail.com>
2022-09-08 08:50:06 +0900

    design: change background Images and style - Modify the style so that the background image fills the screen

--- js/background.js
+++ js/background.js
@@ -1,8 +1,5 @@
-const images = ["1.jpeg", "2.jpeg", "3.jpeg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
